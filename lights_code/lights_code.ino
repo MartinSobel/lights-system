e86245ebbde13568c14d17f011e414c9ba8c73f0
Martín Sobel <marto.sobel@gmail.com>
2024-12-02 17:24:09 -0300

    update code

--- lights_code/lights_code.ino
+++ lights_code/lights_code.ino
@@ -1,14 +1,14 @@
 #include "MIDIUSB.h"
 
 const byte pinArray[] = {2, 3, 4, 5, 6, 7, 8, 9, 21, 20, 19, 18, 15, 14, 16, 10};
-const byte numPins = sizeof(pinArray) / sizeof(pinArray[0]);
+const byte numPins = sizeof(pinArray)
 const byte startNote = 24;
 
 void setup() {
   Serial.begin(115200);
   for (byte i = 0; i < numPins; i++) {
     pinMode(pinArray[i], OUTPUT);
-    digitalWrite(pinArray[i], LOW); // Inicializar en LOW
+    digitalWrite(pinArray[i], LOW);
   }
 }
 
@@ -25,9 +25,9 @@ void loop() {
     rx = MidiUSB.read();
     if (rx.header != 0) {
       if (rx.byte2 >= startNote && rx.byte2 < startNote + numPins) {
-        if (rx.header == 8) {        // Note Off
+        if (rx.header == 8) {
           handleNoteEvent(rx.byte2, false);
-        } else if (rx.header == 9) { // Note On
+        } else if (rx.header == 9) {
           handleNoteEvent(rx.byte2, true);
         }
       }
